ce9b2d4ef7221e450607e1ca9f57f3e1c1a59164
Nikita Chaika <iasastudent2021@gmail.com>
2023-05-08 23:56:18 +0300

    back-end with NextAPI

--- helpers/db-util.js
+++ helpers/db-util.js
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 
 export async function connectDatabase() {
   const client = await MongoClient.connect(
-    "mongodb+srv://MykytaChaika:hyper2003@todolist.he0tlqk.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://MykytaChaika:hyper2003@todolist.he0tlqk.mongodb.net/NextEvent?retryWrites=true&w=majority"
   );
 
   return client;
